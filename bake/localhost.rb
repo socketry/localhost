bf11823e9b5dc6414f83bad49a1a26aef98e8fc4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-09-20 10:58:00 +1200

    Add bake task for listing certificates.

---
+++ bake/localhost.rb
@@ -0,0 +1,31 @@
+
+# List all local authorities.
+def list
+	require_relative '../lib/localhost'
+	
+	terminal = self.terminal
+	
+	Localhost::Authority.list do |authority|
+		terminal.print(
+			:hostname, authority.hostname, " ",
+			:name, authority.name, "\n", :reset,
+			"\tCertificate Path: ", authority.certificate_path, "\n",
+			"\t        Key Path: ", authority.key_path, "\n",
+			"\t         Expires: ", authority.certificate.not_after, "\n",
+			:reset, "\n"
+		)
+	end
+end
+
+private
+
+def terminal(out = $stdout)
+	require 'console/terminal'
+	
+	terminal = Console::Terminal.for(out)
+	
+	terminal[:hostname] = terminal.style(nil, nil, :bold)
+	terminal[:name] = terminal.style(:blue)
+	
+	return terminal
+end
