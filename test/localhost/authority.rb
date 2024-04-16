5019627b77dd4aa04297051c0ed4a8e861e3ebe6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-16 16:49:47 +1200

    Improve handling of old `~/.localhost` directory.
    - Ensure tests are idempotent.

--- test/localhost/authority.rb
+++ test/localhost/authority.rb
@@ -17,30 +17,42 @@ require 'fileutils'
 require 'tempfile'
 
 describe Localhost::Authority do
-	def before
-		@old_root = File.expand_path("~/.localhost")
-		@old_root_exists = File.directory?(@old_root)
-
-		if @old_root_exists
-			@tmp_folder = File.expand_path("~/.localhost_test")
-			FileUtils.mkdir_p(@tmp_folder, mode: 0700)
-			FileUtils.cp_r("#{@old_root}/.", @tmp_folder)
+	def around
+		Dir.mktmpdir do |path|
+			@root = path
+			
+			yield
+		ensure
+			@root = nil
 		end
 	end
-
-	def after
-		if @old_root_exists
-			FileUtils.mkdir_p(@old_root, mode: 0700)
-			FileUtils.mv("#{@tmp_folder}/.", @old_root, force: true)
-			FileUtils.rm_r(@tmp_folder)
+	
+	let(:authority) {subject.new("localhost", root: @root)}
+	
+	with ".path" do
+		it "uses XDG_STATE_HOME" do
+			env = {'XDG_STATE_HOME' => @root}
+			
+			expect(Localhost::Authority.path(env)).to be == File.expand_path("localhost.rb", @root)
+		end
+		
+		it "copies legacy directory" do
+			xdg_state_home = File.join(@root, ".local", "state")
+			env = {'XDG_STATE_HOME' => xdg_state_home}
+			
+			old_root = File.join(@root, ".localhost")
+			Dir.mkdir(old_root)
+			File.write(File.join(old_root, "localhost.crt"), "*fake certificate*")
+			File.write(File.join(old_root, "localhost.key"), "*fake key*")
+			
+			path = Localhost::Authority.path(env, old_root: old_root)
+			expect(path).to be == File.expand_path("localhost.rb", xdg_state_home)
+			expect(File).to be(:exist?, File.expand_path("localhost.crt", path))
+			expect(File).to be(:exist?, File.expand_path("localhost.key", path))
+			
+			expect(File).not.to be(:exist?, old_root)
 		end
 	end
-
-	let(:xdg_dir) { File.join(Dir.pwd, "state") }
-	let(:authority) {
-		ENV["XDG_STATE_HOME"] = xdg_dir
-		subject.new
-	}
 	
 	with '#certificate' do
 		it "is not valid for more than 1 year" do
@@ -52,35 +64,15 @@ describe Localhost::Authority do
 		end
 	end
 	
-	it "can generate key and certificate" do
-		Dir.mktmpdir('localhost') do |dir|
-			authority.save(dir)
-			
-			expect(File).to be(:exist?, File.expand_path("localhost.lock", dir))
-			expect(File).to be(:exist?, File.expand_path("localhost.crt", dir))
-			expect(File).to be(:exist?, File.expand_path("localhost.key", dir))
-		end
-	end
-	
 	it "have correct key and certificate path" do
-		authority.save(authority.class.path)
+		authority.save
+		
 		expect(File).to be(:exist?, authority.certificate_path)
 		expect(File).to be(:exist?, authority.key_path)
 		
-		expect(authority.key_path).to be == File.join(xdg_dir, "localhost.rb", "localhost.key")
-		expect(authority.certificate_path).to be == File.join(xdg_dir, "localhost.rb", "localhost.crt")
-	end
-	
-	it "properly falls back when XDG_STATE_HOME is not set" do
-		ENV.delete("XDG_STATE_HOME")
-		authority = subject.new
-		
-		authority.save(authority.class.path)
-		expect(File).to be(:exist?, authority.certificate_path)
-		expect(File).to be(:exist?, authority.key_path)
-		
-		expect(authority.key_path).to be == File.join(File.expand_path("~/.local/state/"), "localhost.rb", "localhost.key")
-		expect(authority.certificate_path).to be == File.join(File.expand_path("~/.local/state/"), "localhost.rb", "localhost.crt")
+		expect(File).to be(:exist?, File.expand_path("localhost.lock", @root))
+		expect(File).to be(:exist?, File.expand_path("localhost.crt", @root))
+		expect(File).to be(:exist?, File.expand_path("localhost.key", @root))
 	end
 	
 	with '#store' do
@@ -92,42 +84,6 @@ describe Localhost::Authority do
 	with '#server_context' do
 		it "can generate appropriate ssl context" do
 			expect(authority.server_context).to be_a OpenSSL::SSL::SSLContext
-		end
-	end
-	
-	with 'client/server' do
-		include Sus::Fixtures::Async::ReactorContext
-		
-		let(:endpoint) {Async::IO::Endpoint.tcp("localhost", 4040)}
-		let(:server_endpoint) {Async::IO::SSLEndpoint.new(endpoint, ssl_context: authority.server_context)}
-		let(:client_endpoint) {Async::IO::SSLEndpoint.new(endpoint, ssl_context: authority.client_context)}
-		
-		let(:client) {client_endpoint.connect}
-		
-		def before
-			@bound_endpoint = Async::IO::SharedEndpoint.bound(server_endpoint)
-			
-			@server_task = reactor.async do
-				@bound_endpoint.accept do |peer|
-					peer.write("Hello World!")
-					peer.close
-				end
-			end
-			
-			super
-		end
-		
-		def after
-			@server_task&.stop
-			@bound_endpoint&.close
-			
-			super
-		end
-		
-		it "can verify peer" do
-			expect(client.read(12)).to be == "Hello World!"
-			
-			client.close
 		end
 	end
 end
