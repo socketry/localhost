13da8467d12064981afac873580a8090afb94fb5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-16 17:25:57 +1200

    100% code coverage.

--- test/localhost/authority.rb
+++ test/localhost/authority.rb
@@ -21,13 +21,24 @@ describe Localhost::Authority do
 		Dir.mktmpdir do |path|
 			@root = path
 			
-			yield
+			super
 		ensure
 			@root = nil
 		end
 	end
 	
 	let(:authority) {subject.new("localhost", root: @root)}
+	
+	it "have correct key and certificate path" do
+		authority.save
+		
+		expect(File).to be(:exist?, authority.certificate_path)
+		expect(File).to be(:exist?, authority.key_path)
+		
+		expect(File).to be(:exist?, File.expand_path("localhost.lock", @root))
+		expect(File).to be(:exist?, File.expand_path("localhost.crt", @root))
+		expect(File).to be(:exist?, File.expand_path("localhost.key", @root))
+	end
 	
 	with ".path" do
 		it "uses XDG_STATE_HOME" do
@@ -64,15 +75,36 @@ describe Localhost::Authority do
 		end
 	end
 	
-	it "have correct key and certificate path" do
-		authority.save
+	with "#dh_key" do
+		it "is a DH key" do
+			expect(authority.dh_key).to be_a OpenSSL::PKey::DH
+		end
+	end
+	
+	with '#name' do
+		it "can get name" do
+			expect(authority.name.to_s).to be == "/O=Development/CN=localhost"
+		end
 		
-		expect(File).to be(:exist?, authority.certificate_path)
-		expect(File).to be(:exist?, authority.key_path)
+		it "can set name" do
+			authority.name = OpenSSL::X509::Name.parse("/CN=example.localhost")
+			expect(authority.name.to_s).to be == "/CN=example.localhost"
+		end
+	end
+	
+	with '#key' do
+		it "is an RSA key" do
+			expect(authority.key).to be_a OpenSSL::PKey::RSA
+		end
 		
-		expect(File).to be(:exist?, File.expand_path("localhost.lock", @root))
-		expect(File).to be(:exist?, File.expand_path("localhost.crt", @root))
-		expect(File).to be(:exist?, File.expand_path("localhost.key", @root))
+		it "can set key" do
+			# Avoid generating a key, it's slow...
+			# key = OpenSSL::PKey::RSA.new(1024)
+			key = authority.key
+			
+			authority.key = key
+			expect(authority.key).to be_equal(key)
+		end
 	end
 	
 	with '#store' do
@@ -84,6 +116,49 @@ describe Localhost::Authority do
 	with '#server_context' do
 		it "can generate appropriate ssl context" do
 			expect(authority.server_context).to be_a OpenSSL::SSL::SSLContext
+		end
+	end
+	
+	with '.list' do
+		def before
+			super
+			
+			authority.save
+		end
+		
+		it "can list all authorities" do
+			authorities = Localhost::Authority.list(@root).to_a
+			
+			expect(authorities.size).to be == 1
+			expect(authorities.first).to be_a Localhost::Authority
+			expect(authorities.first).to have_attributes(
+				hostname: be == "localhost",
+			)
+		end
+	end
+	
+	with '.fetch' do
+		def before
+			super
+			
+			authority.save
+		end
+		
+		it "can fetch existing authority" do
+			fetched_authority = Localhost::Authority.fetch("localhost", root: @root)
+			expect(fetched_authority).to have_attributes(
+				hostname: be == "localhost",
+			)
+		end
+		
+		it "can create new authority" do
+			fetched_authority = Localhost::Authority.fetch("example.com", root: @root)
+			expect(fetched_authority).to have_attributes(
+				hostname: be == "example.com",
+			)
+			
+			expect(File).to be(:exist?, fetched_authority.certificate_path)
+			expect(File).to be(:exist?, fetched_authority.key_path)
 		end
 	end
 end
