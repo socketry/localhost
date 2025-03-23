de9d07b3cd73fe6e225a8960190ff7ebc53be6bb
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-03-23 21:58:47 +1300

    Add better documentation to bake tasks.

--- bake/localhost.rb
+++ bake/localhost.rb
@@ -10,6 +10,8 @@ def initialize(...)
 end
 
 # List all local authorities.
+#
+# @returns [Array(Hash)] The certificate and key paths, and the expiry date.
 def list
 	Localhost::Authority.list.map do |authority|
 		{
@@ -20,6 +22,10 @@ def list
 	end
 end
 
+# Fetch a local authority by hostname. If the authority does not exist, it will be created.
+#
+# @parameter hostname [String] The hostname to fetch.
+# @returns [Hash] The certificate and key paths, and the expiry date.
 def fetch(hostname)
 	if authority = Localhost::Authority.fetch(hostname)
 		return {
