8cf75ecc871d23c2739dd5afa94660cc62e1c09e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-03-23 20:58:13 +1300

    Updated bake tasks.

--- bake/localhost.rb
+++ bake/localhost.rb
@@ -3,33 +3,29 @@
 # Released under the MIT License.
 # Copyright, 2021-2023, by Samuel Williams.
 
+def initialize(...)
+	super
+	
+	require_relative "../lib/localhost"
+end
+
 # List all local authorities.
 def list
-	require_relative "../lib/localhost"
-	
-	terminal = self.terminal
-	
-	Localhost::Authority.list do |authority|
-		terminal.print(
-			:hostname, authority.hostname, " ",
-			:name, authority.name, "\n", :reset,
-			"\tCertificate Path: ", authority.certificate_path, "\n",
-			"\t        Key Path: ", authority.key_path, "\n",
-			"\t         Expires: ", authority.certificate.not_after, "\n",
-			:reset, "\n"
-		)
+	Localhost::Authority.list.map do |authority|
+		{
+			certificate_path: authority.certificate_path,
+			key_path: authority.key_path,
+			expires_at: authority.certificate.not_after,
+		}
 	end
 end
 
-private
-
-def terminal(out = $stdout)
-	require "console/terminal"
-	
-	terminal = Console::Terminal.for(out)
-	
-	terminal[:hostname] = terminal.style(nil, nil, :bold)
-	terminal[:name] = terminal.style(:blue)
-	
-	return terminal
+def fetch(hostname)
+	if authority = Localhost::Authority.fetch(hostname)
+		return {
+			certificate_path: authority.certificate_path,
+			key_path: authority.key_path,
+			expires_at: authority.certificate.not_after,
+		}
+	end
 end
