fd79fc1eb9c34eada2ddb904968c0db2d3cf8257
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-05-02 10:11:20 +0900

    Modernize code.

--- bake.rb
+++ bake.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2025, by Samuel Williams.
+# Copyright, 2025-2026, by Samuel Williams.
 
 # Update the project documentation with the new version number.
 #
 # @parameter version [String] The new version number.
 def after_gem_release_version_increment(version)
 	context["releases:update"].call(version)
-	context["utopia:project:readme:update"].call
+	context["utopia:project:update"].call
+end
+
+# Create a GitHub release for the given tag.
+#
+# @parameter tag [String] The tag to create a release for.
+def after_gem_release(tag:, **options)
+	context["releases:github:release"].call(tag)
 end
--- localhost.gemspec
+++ localhost.gemspec
@@ -22,5 +22,5 @@ Gem::Specification.new do |spec|
 	
 	spec.files = Dir.glob(["{bake,lib}/**/*", "*.md"], File::FNM_DOTMATCH, base: __dir__)
 	
-	spec.required_ruby_version = ">= 3.2"
+	spec.required_ruby_version = ">= 3.3"
 end
